946a22d959d7599d4708d71619feb191617b5d99
Aaron Ranard <aaronranard@gmail.com>
2017-08-04 11:55:39 -0600

    add an active--chapter class to the li if its submenu is selected

---
+++ book/nio.js
@@ -0,0 +1,19 @@
+require(["gitbook"], function(gitbook) {
+    gitbook.events.bind("page.change", function(event) {
+    	// Remove active chapter globally
+     	$('ul.summary li').removeClass('active--chapter');
+    	// this will give us the selected sub-menus level
+     	const level = $('ul.summary li li.active').data('level'); 
+     	if (level) {
+	     	// find the position of the second period
+	     	const dot = level.indexOf('.', 2);
+	     	// the parent is the substring up until the second period
+	     	const parent = level.substring(0, dot);
+     		$('ul.summary li[data-level="'+parent+'"]').addClass('active--chapter');
+     	}
+    });
+
+    gitbook.events.bind("exercise.submit", function() {
+        // do something
+    });
+});
--- book/test.js
+++
@@ -1,9 +0,0 @@
-require(["gitbook"], function(gitbook) {
-    gitbook.events.bind("page.change", function(event) {
-        // do something
-    });
-
-    gitbook.events.bind("exercise.submit", function() {
-        // do something
-    });
-});
