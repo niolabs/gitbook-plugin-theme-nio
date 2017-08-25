84ac876704dc6b7b4a661da6025a254d678aac97
Aaron Ranard <aaronranard@gmail.com>
2017-08-25 14:56:06 -0600

    add animate hack

--- book/nio.js
+++ book/nio.js
@@ -9,7 +9,7 @@ require(["gitbook"], function(gitbook) {
      	$('ul.summary li').removeClass('active--chapter');
     	// this will give us the selected sub-menus level
       var level = $('ul.summary li li.active').data('level');
-      console.log(level);
+
      	if (level) {
 	     	// find the position of the first period after the 2nd index (which won't work for three-digit levels)
 	     	var dot = level.indexOf('.', 2);
@@ -17,6 +17,12 @@ require(["gitbook"], function(gitbook) {
 	     	var parent = level.substring(0, dot);
      		$('ul.summary li[data-level="'+parent+'"]').addClass('active--chapter');
      	}
+
+      // This is a hack in order to get the sidebar animation working
+      $('ul.summary > li.active').addClass('animating');
+      setTimeout(function() {
+        $('ul.summary > li').removeClass('animating');
+      }, 50);
     });
 
     gitbook.events.bind("exercise.submit", function() {
